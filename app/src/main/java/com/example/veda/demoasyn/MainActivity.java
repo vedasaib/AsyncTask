1a35f8b974fdb7a5135fc38bd149fa4a795e1a7b
vedasaib <Annapurna1@>
2018-10-14 23:50:02 +0530

    initial commit

---
+++ app/src/main/java/com/example/veda/demoasyn/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.veda.demoasyn;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
+
+
+        class MultiTask extends AsyncTask<String,String,String>{
+
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+            }
+
+            @Override
+            protected String doInBackground(String... strings) {
+                return null;
+            }
+        }
