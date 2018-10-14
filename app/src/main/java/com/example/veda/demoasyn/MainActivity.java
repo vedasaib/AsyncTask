6ddb9daa1187ac0006da77400b0b0a1f2f5326d9
vedasaib <Annapurna1@>
2018-10-15 00:13:55 +0530

    added URL conncetion

--- app/src/main/java/com/example/veda/demoasyn/MainActivity.java
+++ app/src/main/java/com/example/veda/demoasyn/MainActivity.java
@@ -4,7 +4,17 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class MainActivity extends AppCompatActivity {
+
+   private String strUrl = "http://www.telsuko.com/addition.htm?t1=3&t2=6";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,24 +23,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    class MultiTask extends AsyncTask<String,String,String>{
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+                URL url = new URL(strUrl);
+                HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+                con.setRequestMethod("GET");
+                con.connect();
+
+                BufferedReader bf = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                String value = bf.readLine();
+                System.out.println("Result" + value);
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            {
+                System.out.println("hai");
+            }
+            return null;
+        }
+    }
+
+
 }
 
 
-        class MultiTask extends AsyncTask<String,String,String>{
 
-
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-            }
-
-            @Override
-            protected void onPostExecute(String s) {
-                super.onPostExecute(s);
-            }
-
-            @Override
-            protected String doInBackground(String... strings) {
-                return null;
-            }
-        }
